c5ca4e3f312f1cdccfa61df426b9ae0483a4de84
Chad Boyce <chadboyce@gmail.com>
2022-10-09 10:11:35 +0000

    bug fix: https://github.com/nix-community/home-manager/issues/2564#issuecomment-994943471

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels
+
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
